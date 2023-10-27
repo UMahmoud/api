6e8e6755ec76ac76d3ffb9b8b83d4b5c76abc0ec
Usman Mahmoud Ilyas <37049946+UMahmoud@users.noreply.github.com>
2023-10-27 22:42:23 +0100

    Update submit.js

--- submit.js
+++ submit.js
@@ -17,12 +17,40 @@ module.exports = async (req, res) => {
       lokacinKaratu,
     } = req.body;
 
-    // Set up a MongoDB connection
-    const uri = "mongodb+srv://umid:%4'6uN9G;H@@~sz@mbala.epvt0ry.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    // // Set up a MongoDB connection
+    // const uri = "mongodb+srv://umid:%4'6uN9G;H@@~sz@mbala.epvt0ry.mongodb.net/?retryWrites=true&w=majority";
+    // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    // Connect to the MongoDB database
+    // // Connect to the MongoDB database
+    // await client.connect();
+
+    
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://umid:umid@mbala.epvt0ry.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("Mbala").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
     // Select the MongoDB collection where you want to insert the data
     const collection = client.db('Mbala').collection('bio_data');
